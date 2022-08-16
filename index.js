6fed49cbb3228c40eb783167e38e22114d21501b
Mo Jiwa <mohammed.jiwa@gmail.com>
2022-08-16 14:42:18 +0100

    remove old code

--- index.js
+++ index.js
@@ -64,10 +64,3 @@ client.on("messageCreate", (message) => {
 
 
 client.login(TOKEN);
-
-// keep awake 
-var http = require("http");
-setInterval(function() {
-    http.get("https://succu-bot.herokuapp.com/");
-    console.log('Keeping alive');
-}, 300000); // every 5 minutes (300000)
